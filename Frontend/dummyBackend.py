3d0b81a2033ec66ac350f3b1c0594da896d5f9db
UEFI-code <zhuoshizhang@hotmail.com>
2024-02-26 14:00:16 +0800

    Add JSON encoder for response serialization

--- Frontend/dummyBackend.py
+++ Frontend/dummyBackend.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+json_encoder = json.JSONEncoder()
+
 @app.route('/chat_test', methods=['POST'])
 def chat_test():
     data = json.loads(request.get_data())
@@ -12,7 +14,7 @@ def chat_test():
     print('Model: ' + data['model'])
     print('Prompt: ' + data['prompt'])
     response = {'status': input('type status: '), 'message': input('type response: ')}
-    response = json.dumps(response)
+    response = json_encoder.encode(response)
     return response, 200
 
 if __name__ == '__main__':
